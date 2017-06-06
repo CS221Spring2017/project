0d229d937d68e70baa1f492e2d9a2d5c1539918b
YutingWang <wytthuss@126.com>
2017-06-06 09:55:52 -0700

    change cache size

--- File/cache_size.c
+++ File/cache_size.c
@@ -10,13 +10,13 @@ static inline unsigned long long rdtsc(void) {
     return (((unsigned long long)hi << 32) | (unsigned long long)lo);
 }
 
-const off_t BLOCK_SIZE = 4 * 1024;
+
 
 int main(int argc, const char* argv[])
 {
 	if(argc < 2)
 	{
-		printf("usage: ./cache 4 M cache4M\n");
+		printf("usage: ./cache 4 M\n");
 		exit(0);
 	}
 	off_t FILE_SIZE = atoll(argv[1]);
@@ -26,21 +26,25 @@ int main(int argc, const char* argv[])
 	if(type == 'M')
 		FILE_SIZE = FILE_SIZE * 1024 * 1024;
 
+	off_t BLOCK_SIZE = 4 * 1024 * 1024;
 	void *buffer = malloc(BLOCK_SIZE);
+
 	unsigned long long total_bytes = 0;
 	unsigned long long begin;
 	unsigned long long end;
 	unsigned long long total;
 
 	//read file into cache
-	int file = open(argv[3], O_RDONLY);	
+	int file = open("data/cache", O_RDONLY | O_SYNC);	
+	
 	if(lseek(file, FILE_SIZE -1, SEEK_SET) == -1)
 	{
 		close(file);
 		return -1;
 	}
+
 	while(1){
-		lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
+		//lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
 		ssize_t bytes = read(file, buffer, BLOCK_SIZE);
 		if(bytes <= 0 || total_bytes >= FILE_SIZE)
 			break;
@@ -49,15 +53,17 @@ int main(int argc, const char* argv[])
 	close(file);
     
 	//Read the file again
-	file = open(argv[3], O_RDONLY);
+	file = open("data/cache", O_RDONLY | O_SYNC);
+	/*
 	if(lseek(file, FILE_SIZE -1, SEEK_SET) == -1)
 	{
 		close(file);
 		return -1;
 	}
+	*/
 	while(1)
 	{
-		lseek(file, -2*BLOCK_SIZE, SEEK_CUR);
+		//lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
 		begin = rdtsc();
 		ssize_t bytes = read(file, buffer, BLOCK_SIZE);
 		end = rdtsc();
@@ -69,6 +75,6 @@ int main(int argc, const char* argv[])
 	close(file);
 	free(buffer);
 
-	double answer = total*1.0/(FILE_SIZE*1.0/BLOCK_SIZE);
-	printf("file size = %d MB, cycle = %lf \n", (int)FILE_SIZE/(1024*1024), answer);
+	double answer = total*1.0/(FILE_SIZE/BLOCK_SIZE);
+	printf("cycle = %lf \n", answer);
 }
