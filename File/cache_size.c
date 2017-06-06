56fcf68e56965ddf7542a97db54eb60e87dacfb0
Cranbeerry <song.842@osu.edu>
2017-06-06 10:30:56 -0700

    updats

--- File/cache_size.c
+++ File/cache_size.c
@@ -14,67 +14,68 @@ static inline unsigned long long rdtsc(void) {
 
 int main(int argc, const char* argv[])
 {
-	if(argc < 2)
-	{
-		printf("usage: ./cache 4 M\n");
-		exit(0);
-	}
-	off_t FILE_SIZE = atoll(argv[1]);
-	const char type = *argv[2];
-	if(type == 'G')	
-		FILE_SIZE = FILE_SIZE * 1024 * 1024 * 1024;
-	if(type == 'M')
-		FILE_SIZE = FILE_SIZE * 1024 * 1024;
-
-	off_t BLOCK_SIZE = 4 * 1024 * 1024;
-	void *buffer = malloc(BLOCK_SIZE);
-
-	unsigned long long total_bytes = 0;
-	unsigned long long begin;
-	unsigned long long end;
-	unsigned long long total;
-
-	//read file into cache
-	int file = open("data/cache", O_RDONLY | O_SYNC);	
-	
-	if(lseek(file, FILE_SIZE -1, SEEK_SET) == -1)
-	{
-		close(file);
-		return -1;
-	}
-
-	while(1){
-		//lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
-		ssize_t bytes = read(file, buffer, BLOCK_SIZE);
-		if(bytes <= 0 || total_bytes >= FILE_SIZE)
-			break;
-		total_bytes += bytes;
-	}
-	close(file);
+    if(argc < 2)
+    {
+        printf("usage: ./cache 4 M\n");
+        exit(0);
+    }
+    off_t FILE_SIZE = atoll(argv[1]);
+    const char type = *argv[2];
+    if(type == 'G')
+        FILE_SIZE = FILE_SIZE * 1024 * 1024 * 1024;
+    if(type == 'M')
+        FILE_SIZE = FILE_SIZE * 1024 * 1024;
     
-	//Read the file again
-	file = open("data/cache", O_RDONLY | O_SYNC);
-	/*
-	if(lseek(file, FILE_SIZE -1, SEEK_SET) == -1)
-	{
-		close(file);
-		return -1;
-	}
-	*/
-	while(1)
-	{
-		//lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
-		begin = rdtsc();
-		ssize_t bytes = read(file, buffer, BLOCK_SIZE);
-		end = rdtsc();
-		total += end - begin;
-		if(bytes <= 0 || total_bytes >= FILE_SIZE)
-			break;
-		total_bytes += bytes;
-	}
-	close(file);
-	free(buffer);
-
-	double answer = total*1.0/(FILE_SIZE/BLOCK_SIZE);
-	printf("cycle = %lf \n", answer);
+    off_t BLOCK_SIZE = 4 * 1024 * 1024;
+    void *buffer = malloc(BLOCK_SIZE);
+    
+    unsigned long long total_bytes = 0;
+    unsigned long long begin;
+    unsigned long long end;
+    unsigned long long total = 0;
+    
+    //read file into cache
+    int file = open("data/cache", O_RDONLY | O_SYNC);
+    /*
+    if(lseek(file, FILE_SIZE -1, SEEK_SET) == -1)
+    {
+        close(file);
+        return -1;
+    }
+    */
+    while(1){
+        //lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
+        ssize_t bytes = read(file, buffer, BLOCK_SIZE);
+        if(bytes <= 0 || total_bytes >= FILE_SIZE)
+            break;
+        total_bytes += bytes;
+    }
+    close(file);
+    
+    //Read the file again
+    file = open("data/cache", O_RDONLY | O_SYNC);
+    total_bytes = 0;
+    /*
+     if(lseek(file, FILE_SIZE -1, SEEK_SET) == -1)
+     {
+     close(file);
+     return -1;
+     }
+     */
+    while(1)
+    {
+        lseek(file, -2 * BLOCK_SIZE, SEEK_CUR);
+        begin = rdtsc();
+        ssize_t bytes = read(file, buffer, BLOCK_SIZE);
+        end = rdtsc();
+        total += end - begin;
+        if(bytes <= 0 || total_bytes >= FILE_SIZE)
+            break;
+        total_bytes += bytes;
+    }
+    close(file);
+    free(buffer);
+    
+    double answer = total*1.0/(FILE_SIZE/BLOCK_SIZE);
+    printf("cycle = %lf \n", answer);
 }
